9520d373d71b6b9dc7b2b8840d25ffdf42b06c7a
JaiKumar78 <your.email@example.com>
2024-07-21 11:37:57 +0530

    adding elements and styles to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,100 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import house_img from '../assets/images/img-3.jpg'
+import about_img from '../assets/images/img-9.jpg'
 import './home.css'
 
 const Home = () => {
   return (
-    <div>
-      <div className="hero">
+    <div className='Home'>
+      <section className="hero">
         <div className="hero-content">
           <h3>"Construction is not just about structures; it’s about building dreams."</h3>
           {/* <hr /> */}
           <div className="line"></div>
           <p>Build it with Us!</p>
         </div>
-      </div>
+      </section>
+      <section className='query'>
+        <div>
+          <p>Are you looking for Construction and Industrial Experts?</p>
+          <Link>Contact Us</Link>
+        </div>
+      </section>
+      <section className='about'>
+        <h3>What we do?</h3>
+        <div className="info">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam ipsum, odit cumque assumenda explicabo recusandae, dolorum quas illum error quia maiores quaerat beatae facilis excepturi. Beatae mollitia reiciendis perferendis molestias?
+            In hic laudantium maxime facere! Dolor aperiam quam expedita corporis, officiis praesentium consequuntur, commodi nihil beatae quas libero molestiae quos aliquam esse sunt natus sapiente distinctio vel! Rem, ab id.
+            Amet perspiciatis, ipsam facere eos debitis optio doloribus, dolore expedita minima non quod dicta atque explicabo tempora esse nemo iure nobis culpa labore dolorum nostrum excepturi distinctio incidunt. Inventore, deleniti.
+          </p>
+          <img src={about_img} alt="" />
+        </div>
+      </section>
+      <section className='our_services'>
+        <h3>Our Services</h3>
+        <div className="sercice_grid">
+          <div className="grid-1">
+            <h4>New Construction</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae placeat maxime blanditiis officia neque harum aliquam! Ducimus, doloribus neque. Omnis quaerat repellendus nisi doloribus vel accusamus ea rem hic odit!</p>
+          </div>
+
+          <div className="grid-2">
+            <h4>New Construction</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae placeat maxime blanditiis officia neque harum aliquam! Ducimus, doloribus neque. Omnis quaerat repellendus nisi doloribus vel accusamus ea rem hic odit!</p>
+          </div>
+
+          <div className="grid-3">
+            <h4>New Construction</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae placeat maxime blanditiis officia neque harum aliquam! Ducimus, doloribus neque. Omnis quaerat repellendus nisi doloribus vel accusamus ea rem hic odit!</p>
+          </div>
+
+          <div className="grid-4">
+            <h4>New Construction</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae placeat maxime blanditiis officia neque harum aliquam! Ducimus, doloribus neque. Omnis quaerat repellendus nisi doloribus vel accusamus ea rem hic odit!</p>
+          </div>
+
+          <div className="grid-5">
+            <h4>New Construction</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae placeat maxime blanditiis officia neque harum aliquam! Ducimus, doloribus neque. Omnis quaerat repellendus nisi doloribus vel accusamus ea rem hic odit!</p>
+          </div>
+
+          <div className="grid-6">
+            <h4>New Construction</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae placeat maxime blanditiis officia neque harum aliquam! Ducimus, doloribus neque. Omnis quaerat repellendus nisi doloribus vel accusamus ea rem hic odit!</p>
+          </div>
+        </div>
+      </section>
+      <section className="expertise">
+        <div className="experience_cards">
+          <div className="card">
+            <h1>85+</h1>
+            <p>Happy clients</p>
+          </div>
+          <div className="card">
+            <h1>85+</h1>
+            <p>Happy clients</p>
+          </div>
+          <div className="card">
+            <h1>85+</h1>
+            <p>Happy clients</p>
+          </div>
+          <div className="card">
+            <h1>85+</h1>
+            <p>Happy clients</p>
+          </div>
+          <div className="card">
+            <h1>85+</h1>
+            <p>Happy clients</p>
+          </div>
+        </div>
+        <div className="experience">
+          <h1>31 Years of Experience</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic labore quibusdam delectus eum magnam dolores minus rerum cupiditate fugiat nobis officia laboriosam libero, recusandae reiciendis ut obcaecati eaque illo at!</p>
+          <button><Link>Contact Us</Link></button>
+        </div>
+      </section>
     </div>
   )
 }
